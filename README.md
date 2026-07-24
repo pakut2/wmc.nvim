a1b71a2050da36f988bf8d313816cae6be3aad38
Paweł Kutny <kutny.pawel@gmail.com>
2026-07-24 21:29:48 +0200

    chore: test

--- README.md
+++ README.md
@@ -12,8 +12,17 @@
 
 Tests are executed with Neovim as the Lua interpreter with [busted](https://lunarmodules.github.io/busted).
 
+1. Install Test Dependencies
+
 ```shell
-chmod +x test.sh
-./test.sh
+luarocks install --tree lua_modules --lua-version 5.1 nlua
+luarocks install --tree lua_modules --lua-version 5.1 busted
+```
+
+2. Run Tests
+
+```shell
+eval "$(luarocks path --tree lua_modules --lua-version 5.1)"
+luarocks test --lua-version 5.1
 ```
 
--- lua/wmc/utils/timer.lua
+++ lua/wmc/utils/timer.lua
@@ -5,26 +5,26 @@ local M = {}
 ---@return uv.uv_timer_t|nil
 ---@return uv.error.message|nil
 function M.set_interval(interval, callback)
-	local timer, err = vim.uv.new_timer()
-	if not timer then
-		return nil, err
-	end
+    local timer, err = vim.uv.new_timer()
+    if not timer then
+        return nil, err
+    end
 
-	local _, err = timer:start(0, interval, function() -- luacheck: ignore 411
-		callback()
-	end)
+	local _, err = timer:start(0, interval, function()
+        callback()
+    end)
 
-	if err then
-		return nil, err
-	end
+    if err then
+        return nil, err
+    end
 
-	return timer, nil
+    return timer, nil
 end
 
 ---@param  timer uv.uv_timer_t
 function M.clear_interval(timer)
-	timer:stop()
-	timer:close()
+    timer:stop()
+    timer:close()
 end
 
 return M
