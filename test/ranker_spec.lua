02f991492af96381dc381de396a1cedbeaf263d9
Paweł Kutny <kutny.pawel@gmail.com>
2026-07-24 20:04:34 +0200

    chore(ranker): unit tests

--- test/ranker_spec.lua
+++ test/ranker_spec.lua
@@ -1,13 +1,23 @@
 describe("ranker", function()
 	local stub = require("luassert.stub")
 	local ui_stub = require("wmc.ui")
-	local timer_stub = require("wmc.utils.timer")
+	local constants = require("wmc.constants")
 
-	local ranker = require("wmc.ranker")
+	local ranker
 
 	local function fill_combo(combo)
 		for i = 1, #combo do
 			ranker.combo:append(combo:sub(i, i))
+		end
+	end
+
+	local function type_combo(combo)
+		local handler = ranker:on_key()
+
+		for i = 1, #combo do
+			local key = combo:sub(i, i)
+
+			handler(key, key)
 		end
 	end
 
@@ -16,11 +26,10 @@ describe("ranker", function()
 			render = function() end,
 			clear = function() end,
 		})
-		stub(timer_stub, "set_interval").returns({})
 	end)
 
 	before_each(function()
-		ranker = ranker:new()
+		ranker = require("wmc.ranker"):new()
 	end)
 
 	after_each(function()
@@ -29,7 +38,63 @@ describe("ranker", function()
 
 	teardown(function()
 		ui_stub.new:revert()
-		timer_stub.set_interval:revert()
+	end)
+
+	describe("on_key", function()
+		it("should assign a rank", function()
+			type_combo("jackpot")
+
+			assert.are.equal(constants.RANK_LABEL.DULL, ranker.rank)
+		end)
+
+		it("should reset idle counter on input", function()
+			ranker.last_input_sec = 1.0
+
+			type_combo("a")
+
+			assert.are.equal(0, ranker.last_input_sec)
+		end)
+
+		it("should advance rank", function()
+			type_combo("jackpotjackpot")
+
+			assert.are.equal(constants.RANK_LABEL.COOL, ranker.rank)
+		end)
+
+		it("should not advance rank for insert mode input", function()
+			stub(vim.api, "nvim_get_mode").returns({ mode = "i" })
+			finally(function()
+				vim.api.nvim_get_mode:revert()
+			end)
+
+			type_combo("jackpot")
+
+			assert.is_nil(ranker.rank)
+			assert.are.equal(0, ranker.combo:get_length())
+			assert.are.equal(0, ranker.last_input_sec)
+		end)
+
+		it("should invalidate rank on mouse usage", function()
+			local mouse_input = vim.api.nvim_replace_termcodes("<LeftMouse>", true, false, true)
+
+			ranker:on_key()(mouse_input, mouse_input)
+
+			assert.are.equal(constants.RANK_LABEL.DULL, ranker.rank)
+			assert.is_false(ranker.is_combo_valid)
+
+			type_combo("jackpotjackpot")
+
+			assert.are.equal(constants.RANK_LABEL.DULL, ranker.rank)
+			assert.is_false(ranker.is_combo_valid)
+		end)
+
+		it("should clear rank after inactivity", function()
+			type_combo("jackpot")
+			vim.wait(1500)
+
+			assert.is_nil(ranker.rank)
+			assert.are.equal(0, ranker.combo:get_length())
+		end)
 	end)
 
 	describe("get_combo_entropy", function()
